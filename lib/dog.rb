5d810f579ad49fbb3bb1cfda88e33e694785fb09
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-14 14:01:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
-class Dog 
-  def initialize(name, breed="mutt")
-    @breed = breed
+class Dog
+  def name
+    @name
+  end
+
+  def name=(name)
     @name = name
   end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
